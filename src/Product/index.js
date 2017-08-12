ffd6c3448a70143aeb85495f2c5920eccbf0fca7
feofan-msk <omail4me@gmail.com>
2017-08-12 11:02:10 +0500

    fix: delete Card component

--- src/Product/index.js
+++ src/Product/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import Recommendations from "./Recommendations";
 import SimilarOffers from "./SimilarOffers";
-import Card from "./Card";
 import Description from "./Description";
 import Shipping from "./Shipping";
 import Delivery from "./Delivery";
+import Gallery from "./Gallery";
+import Info from "./Info";
+import Actions from "./Actions";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 1rem;
+  font-family: 'Lora';
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.2;
+  color: #171717;
+  @media screen and (min-width: 48rem) {
+    margin: 1.5rem 0.5rem 1.5rem 0;
+    font-size: 1.5rem;
+    line-height: 1.75rem;
+  }
+`;
 
 export default () => {
   return (
     <main className="container">
-      <Card />
+      <Title>Long Cotton Gabardine Car Coat</Title>
+
+      <div className="row">
+        <div className="col-xs-12 col-md-7">
+          <Gallery />
+        </div>
+
+        <div className="col-xs-12 col-md-5">
+          <Info />
+
+          <Actions />
+        </div>
+      </div>
       <Description />
       <Shipping />
       <Delivery />
