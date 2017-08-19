aba854bd4bda8d0f43f1615df3d82d0a023c01fa
feofan-msk <omail4me@gmail.com>
2017-08-20 02:20:41 +0500

    fix: good quality for recommendations images

--- src/Products/Show/Recommendations.jsx
+++ src/Products/Show/Recommendations.jsx
@@ -25,7 +25,7 @@ export default () =>
           label="Relaxed fit"
           ColoursAmount={3}
           price={27000}
-          src="https://assets.burberry.com/is/image/Burberryltd/f51a470fc157e3014a115c64276c2e124d9fc477.jpg?$BBY_V2_ML_3X4$&hei=260&wid=195"
+          src="https://assets.burberry.com/is/image/Burberryltd/f51a470fc157e3014a115c64276c2e124d9fc477.jpg?$BBY_V2_ML_3X4$&hei=628&wid=464"
           alt="Recommend 1"
         />
       </div>
@@ -37,7 +37,7 @@ export default () =>
           label="Relaxed fit"
           ColoursAmount={3}
           price={22500}
-          src="https://assets.burberry.com/is/image/Burberryltd/eb872d02449c4050916c9eaf44e266bfdbd32304.jpg?$BBY_V2_ML_3X4$&hei=260&wid=195"
+          src="https://assets.burberry.com/is/image/Burberryltd/eb872d02449c4050916c9eaf44e266bfdbd32304.jpg?$BBY_V2_ML_3X4$&hei=628&wid=464"
           alt="Recommend 2"
         />
       </div>
@@ -49,7 +49,7 @@ export default () =>
           label="Relaxed fit"
           ColoursAmount={3}
           price={120000}
-          src="https://assets.burberry.com/is/image/Burberryltd/cfaeb1b3c79aeee09dfda4e26a9fb3f4525df542.jpg?$BBY_V2_SL_3X4$&hei=260&wid=195"
+          src="https://assets.burberry.com/is/image/Burberryltd/cfaeb1b3c79aeee09dfda4e26a9fb3f4525df542.jpg?$BBY_V2_SL_3X4$&hei=628&wid=464"
           alt="Recommend 3"
         />
       </div>
@@ -61,7 +61,7 @@ export default () =>
           label="Relaxed fit"
           ColoursAmount={3}
           price={46000}
-          src="https://assets.burberry.com/is/image/Burberryltd/b33127f06e6c404cc3168645e099a9eb71aeca9c.jpg?$BBY_V2_SL_3X4$&hei=260&wid=195"
+          src="https://assets.burberry.com/is/image/Burberryltd/b33127f06e6c404cc3168645e099a9eb71aeca9c.jpg?$BBY_V2_SL_3X4$&hei=628&wid=464"
           alt="Recommend 4"
         />
       </div>
