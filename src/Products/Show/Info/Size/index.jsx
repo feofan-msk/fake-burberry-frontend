fe4bda596f9a5e29a4b0394d276ec3df32cace99
feofan-msk <30575877+feofan-msk@users.noreply.github.com>
2017-09-06 16:45:20 +0300

    Feat filters dropdowns (#10)
    * feat: add dropdowns for filters
    
    * feat: make dropdowns
    
    * fix: right dropdown positioning
    
    * feat: add darken under the dropdowns
    
    * fix: right filters positioning
    
    * fix: naming for filters
    
    * fix: non-rotating arrow, files hierarchy, non-greying filters
    
    * fix: overlay size
    
    * fix: lint problem
    
    * fix: syntax improves, refactore nameing
    
    * fix: rename Overlay
    
    * fix: order comparison for filter's state
    
    * Feat color size btns (#11)

---
+++ src/Products/Show/Info/Size/index.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Selector from './Selector';
+
+const availableSizes = ['S', 'M', 'L', 'XL'];
+
+const SizeWrapper = styled.div`
+  display: none;
+  @media screen and (min-width: 62rem) {
+    display: block;
+  }
+`;
+const SelectPanel = styled.div`display: flex;`;
+const Header = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+  margin-bottom: 1rem;
+`;
+const SizeCurrent = styled.div`
+  margin: 0;
+  font-family: Raleway;
+  font-size: 0.75rem;
+  line-height: 0.875rem;
+  font-weight: 400;
+  color: #171717;
+`;
+const TextBtn = styled.button`
+  display: block;
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+
+  font-family: Raleway;
+  font-weight: 400;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  color: #171717;
+`;
+
+class Size extends Component {
+  state = {
+    selectedSizeIndex: 3,
+  };
+
+  handleClick = (newSizeIndex) => {
+    this.setState({ selectedSizeIndex: newSizeIndex });
+  };
+
+  render() {
+    return (
+      <SizeWrapper>
+        <Header>
+          <SizeCurrent>
+            Size:
+            <strong>{availableSizes[this.state.selectedSizeIndex]}</strong>
+          </SizeCurrent>
+
+          <TextBtn>NEED SIZE HELP?</TextBtn>
+        </Header>
+
+        <SelectPanel>
+          {availableSizes.map((size, index) => (
+            <Selector
+              onClick={this.handleClick}
+              name={size}
+              index={index}
+              isActive={this.state.selectedSizeIndex === index}
+            />
+          ))}
+        </SelectPanel>
+      </SizeWrapper>
+    );
+  }
+}
+
+export default Size;
